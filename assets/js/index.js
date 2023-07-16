f8e18eea896cd0c3e5be2a984dd920be50215dd6
mashael farajallah <mashael.qwer@gmail.com>
2023-07-16 13:06:09 +0300

    update

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 5,
+    },
+  },
+});
